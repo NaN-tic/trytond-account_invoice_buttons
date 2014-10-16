46443f91afaec6ebdf9175cdda81962197c86884
resteve <resteve@zikzakmedia.com>
2014-10-16 11:04:21 +0200

    Setup

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = ['proteus >= %s.%s, < %s.%s' %
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='Tryton module add Galatea access in newsletters',
+    description='Tryton module add buttons in tree view',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
